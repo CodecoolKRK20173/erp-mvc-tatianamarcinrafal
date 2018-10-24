9ab52cb7fd7dfa187315490f7b0b599923df1126
Tatiana Slonimskaia <slonimskaia@gmail.com>
2018-10-24 21:25:03 +0200

    model and controller for accounting, changing .csv

--- controller/accounting_controller.py
+++ controller/accounting_controller.py
@@ -2,7 +2,7 @@
 from view import terminal_view
 from model.accounting import accounting
 from controller import common
-from controller import root_controller#new
+from model import data_manager
 
 def run():
     """
@@ -13,24 +13,44 @@ def run():
     Returns:
         None
     """
-    
-    options = ["Show table","Add data", "Remove data", "Update data", "Go back to Main Menu"]
-    
+    title_list = ["ID", "Month", "Day", "Year", "Income(enter: 'in') or Outflow(enter:'out') money", "Amount"]
+    options = ["Show table","Add data", "Remove data", "Update data"]
+    link_for_csv = 'model/accounting/items.csv'
     choice = None
     while choice != "0":
         choice = terminal_view.get_choice(options)
         if choice == "1":
+            table = data_manager.get_table_from_file(link_for_csv)    
             terminal_view.print_table(table, title_list)
         elif choice == "2":
-            accounting.add(table, record)
+            add()
         elif choice == "3":
-            accounting.remove(table, id_)
+            remove()
         elif choice == "4":
-            accounting.update(table, id_, record)
-        elif choice == "5":
-            root_controller.run()
-        elif choice == "6":
-            break
+            update()
         else:
             terminal_view.print_error_message("There is no such choice.")
-    # your code
+
+def add():
+    link_for_csv = 'model/accounting/items.csv'
+    table = data_manager.get_table_from_file(link_for_csv)
+    title_list = ["Month: ", "Day: ", "Year: ", "Income or Outflow money: ", "Amount: "]
+    record = terminal_view.get_inputs(title_list, "Please provide following data:")
+    accounting.add(table, record)
+    data_manager.write_table_to_file(link_for_csv, table)
+
+def remove():
+    link_for_csv = 'model/accounting/items.csv'
+    table = data_manager.get_table_from_file(link_for_csv)
+    id_ = terminal_view.get_inputs(["ID: "], "Please provide ID you want to remove")
+    accounting.remove(table, id_)
+    data_manager.write_table_to_file(link_for_csv, table)
+
+def update():
+    link_for_csv = 'model/accounting/items.csv'
+    table = data_manager.get_table_from_file(link_for_csv)
+    id_ = terminal_view.get_inputs(["ID: "], "Please provide ID you want to edit")
+    title_list = ["Month: ", "Day: ", "Year: ", "Income or Outflow money: ", "Amount: "]
+    record = terminal_view.get_inputs(title_list, "Please provide following data:")
+    accounting.update(table, id_, record)
+    data_manager.write_table_to_file(link_for_csv, table)
