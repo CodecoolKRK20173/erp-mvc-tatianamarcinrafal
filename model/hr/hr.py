7d7da2c583c1bde4e5ed02b318aac9dbb480237e
r4fgacon <r4fgacon@gmail.com>
2018-10-24 13:58:04 +0200

    common

--- model/hr/hr.py
+++ model/hr/hr.py
@@ -8,63 +8,10 @@ Data table structure:
 """
 
 # everything you'll need is imported:
+
+
 from model import data_manager
 from model import common
-
-
-def add(table, record):
-    """
-    Add new record to table
-
-    Args:
-        table (list): table to add new record to
-        record (list): new record
-
-    Returns:
-        list: Table with a new record
-    """
-    record.insert(0, common.generate_random(record))
-    table.append(record)
-
-    return table
-
-
-def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
-
-    Args:
-        table (list): table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        list: Table without specified record.
-    """
-    for element in table:
-        test = None
-        if element[0] == id_[0]:
-            table.remove(element)
-    return table
-
-
-def update(table, id_, record):
-    """
-    Updates specified record in the table.
-
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-        record (list): updated record
-
-    Returns:
-        list: table with updated record
-    """
-    record.insert(0, id_[0])
-    for element in table:
-        if element[0] == id_[0]:
-            table[table.index(element)] = record[:]
-    return table
-
 
 # special functions:
 # ------------------
