9e7e962fa404923f85151a3bb2f6da3d88e00986
r4fgacon <r4fgacon@gmail.com>
2018-10-26 13:32:04 +0200

    working_sales_controller

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -12,10 +12,15 @@ def run():
     file = "model/sales/sales.csv"
     title_list = ["ID", "Title", "Price", "Month", "Day", "Year"]
     choice = None
+    dont_clear = False
     options_which_no_table_show = ['4', '5']
     while choice != '0':
-        table = data_manager.get_table_from_file(file)
+        if not dont_clear:
+            os.system("clear")
+            table = data_manager.get_table_from_file(file)
+            terminal_view.print_table(table, title_list)
         choice = terminal_view.get_choice_submenu(options)
+        dont_clear = False
         if choice not in options_which_no_table_show:
             terminal_view.print_table(table, title_list)
         if choice == '1':
@@ -27,13 +32,12 @@ def run():
         if choice == '4':
             lowest_price = sales.get_lowest_price_item_id(table)
             terminal_view.print_result(lowest_price, 'Lowest price game is: ')
+            dont_clear = True
         if choice == '5':
+            os.system("clear")
             dates_to_input = ['From month: ', 'From day: ', 'From year: ', 'To month:', 'To day: ', "To year: "]
             inputs = terminal_view.get_inputs(dates_to_input, "Please input appropriate data.")
             answer = sales.get_items_sold_between(table, int(inputs[0]), int(inputs[1]), int(inputs[2]), int(inputs[3]),
                                          int(inputs[4]), int(inputs[5]))
             terminal_view.print_table(answer, title_list)
-            # print here
-
-        # else:
-        #     terminal_view.print_error_message("There is no such choice.")
+            dont_clear = True
